ecb135b2406c219b44dc1544dbbfcf7602df57e1
omarelkeshen <oelkeshen5@gmail.com>
2023-06-11 07:43:39 +0300

    update 5

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,11 +12,10 @@ export default class App extends Component {
       <>
       <Navbar />
         <Routes>
-          <Route path='Bookhub' element={<Home />}></Route>
-          <Route path='/' element={<Home />}> 
-            <Route path='about' element={<About />}></Route>
-            <Route path='book' element={<BookList />}></Route>
-            <Route path='book/:id' element={<BookDetails />}></Route>
+          <Route path='Bookhub' element={<Home />}> 
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/book' element={<BookList />}></Route>
+            <Route path='/book/:id' element={<BookDetails />}></Route>
           </Route>
         </Routes>
       </>
