b348703fd26d8161afe2cbb1c8720ccfc79f6adc
omarelkeshen <oelkeshen5@gmail.com>
2023-06-11 08:36:59 +0300

    update 9

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -20,7 +20,6 @@ export default class App extends Component {
             <Route path='' element={<BookList />}></Route>
             <Route path='book/:id' element={<BookDetails />}></Route>
           </Route>
-          <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </>
     )
